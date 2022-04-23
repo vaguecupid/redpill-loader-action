9690845831b146f60397fb09d243aeaa965a3a10
Jinho Yoon <37324479+ek2rlstk@users.noreply.github.com>
2022-04-23 12:30:56 +0900

    vm?real?

--- build-universal-update-7.1.sh
+++ build-universal-update-7.1.sh
@@ -157,5 +157,8 @@ then sudo ./build-loader.sh ${dsmodel} ${major}'-'${os_version}'u'${minor}
 else sudo ./build-loader.sh ${dsmodel} ${major}'-'${os_version}
 fi
 mv images/redpill-${dsmodel}*.img ${root}/output/
-sudo qemu-img convert -O vmdk ${root}/output/redpill-${dsmodel}*.img ${root}/output/redpill-${dsmodel}-${build_para}.vmdk
+if [ $worktarget = "VM" ]; then
+ sudo qemu-img convert -O vmdk ${root}/output/redpill-${dsmodel}*.img ${root}/output/redpill-${dsmodel}-${build_para}.vmdk
+ sudo rm ${root}/output/redpill-${dsmodel}*.img
+fi
 cd ${root}
